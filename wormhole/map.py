2ed9316a77fb21a79b8acffe6e2e95672d346b03
Simone Multari <simone.multari@redergo.com>
2023-05-11 01:24:31 +0200

    double exit

--- wormhole/map.py
+++ wormhole/map.py
@@ -1,7 +1,7 @@
 import re
 import matplotlib.pyplot as plt
 
-def draw_maze(num_righe, num_colonne, pos_iniziale, pos_finale, celle_occupate, wormholes, cammino):
+def draw_maze(num_righe, num_colonne, pos_iniziale, pos_finali, celle_occupate, wormholes, cammino):
     # Creazione del labirinto vuoto
     maze = [[' ' for _ in range(num_colonne + 1 )] for _ in range(num_righe + 1)]
 
@@ -26,9 +26,12 @@ def draw_maze(num_righe, num_colonne, pos_iniziale, pos_finale, celle_occupate, 
 
     # Disegno della posizione iniziale e finale
     pos_iniziale = get_coordinate(pos_iniziale)
-    pos_finale = get_coordinate(pos_finale)
+    for pos_finale in pos_finali:
+        pos = get_coordinate(pos_finale)
+        maze[pos[0]][pos[1]] = 'F'
+
     maze[pos_iniziale[0]][pos_iniziale[1]] = 'S'
-    maze[pos_finale[0]][pos_finale[1]] = 'F'
+    # maze[pos_finale[0]][pos_finale[1]] = 'F'
 
     # Stampa del labirinto
     for riga in maze:
@@ -68,20 +71,21 @@ def get_coordinate(posizione):
     return int(posizione[0]), int(posizione[1])
 
 # Lettura del file di input
-with open('dominio.pl', 'r') as file:
+with open('dominioDoppiaUscita.pl', 'r') as file:
     input_text = file.read()
 # Estrazione delle informazioni utilizzando le regex
 num_righe_match = re.search(r'num_righe\((\d+)\)', input_text)
 num_colonne_match = re.search(r'num_colonne\((\d+)\)', input_text)
 pos_iniziale_match = re.search(r'iniziale\(pos\((\d+),(\d+)\)\)', input_text)
-pos_finale_match = re.search(r'finale\(pos\((\d+),(\d+)\)\)', input_text)
+pos_finale_match = re.findall(r'finale\(pos\((\d+),(\d+)\)\)', input_text)
 celle_occupate_matches = re.findall(r'occupata\(pos\((\d+),(\d+)\)\)', input_text)
 wormholes_matches = re.findall(r'wormhole\(pos\((\d+),(\d+)\),pos\((\d+),(\d+)\)\)', input_text)
 # Estrazione dei valori dalle corrispondenze delle regex
 num_righe = int(num_righe_match.group(1))
 num_colonne = int(num_colonne_match.group(1))
 pos_iniziale = (int(pos_iniziale_match.group(1)), int(pos_iniziale_match.group(2)))
-pos_finale = (int(pos_finale_match.group(1)), int(pos_finale_match.group(2)))
+pos_finali = [(int(x[0]), int(x[1])) for x in pos_finale_match ]
+#pos_finale = (int(pos_finale_match.group(1)), int(pos_finale_match.group(2)))
 celle_occupate = [(int(x[0]), int(x[1])) for x in celle_occupate_matches]
 wormholes = [((int(x[0]), int(x[1])), (int(x[2]), int(x[3]))) for x in wormholes_matches]
 
@@ -90,5 +94,5 @@ with open('cammino.txt', 'r') as file:
 
 cammino = input_text.split(',')
 # Disegno del labirinto
-draw_maze(num_righe, num_colonne, pos_iniziale, pos_finale, celle_occupate, wormholes,cammino)
+draw_maze(num_righe, num_colonne, pos_iniziale, pos_finali, celle_occupate, wormholes,cammino)
 
